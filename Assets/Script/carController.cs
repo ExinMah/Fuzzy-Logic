4a4914aba78e8ca44af491db8b24386dba0f18f8
pakxt <pakxt-wp20@student.tarc.edu.my>
2023-04-27 19:30:58 +0800

    carController

---
+++ Assets/Script/carController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour
+{
+    public float carSpeed;
+    Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+
+        transform.position = position;
+    }
+}
