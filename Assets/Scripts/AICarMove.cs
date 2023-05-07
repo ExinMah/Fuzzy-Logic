31b9b491bd048a22b98f721d44e0bd27c827a2fc
ExinMah <mahe-wp20@student.tarc.edu.my>
2023-05-07 22:52:21 +0800

    Add comments

--- Assets/Scripts/AICarMove.cs
+++ Assets/Scripts/AICarMove.cs
@@ -8,17 +8,20 @@ using Unity.VisualScripting;
 public class AICarMove : MonoBehaviour
 {
     public Transform player;
-    public TextAsset fuzzyLogicData;
-    private FuzzyLogic fuzzyLogic = null;
-    public float speed = 5f;
-    public float distance;
-    public static float lifetime = 0;
-    public static float previousLifetime = 1f;
-    public static float speedMultiplier = 0.05f;
+    public static float lifetime = 0; // for current gameplay
+    public static float speedMultiplier = 0.01f; // to slow down speed from the range of 0 - 100 to 0 - 10
+    
+    // Related to fuzzy logic
+    public TextAsset fuzzyLogicData; // the data file contain all your fuzzy logic
+    private FuzzyLogic fuzzyLogic = null; // the fuzzy logic class constructor
+    public float speed = 5f; // output of fuzzy logic, need to defuzzified
+    public float distance; // an fuzzification, an input for fuzzy logic
+    public static float previousLifetime = 1f; // another fuzzification, another input for fuzzy logic
 
     // Start is called before the first frame update
     void Start()
     {
+        // Deserialize the bytes file to string
         fuzzyLogic = FuzzyLogic.Deserialize(fuzzyLogicData.bytes, null);
     }
 
@@ -28,13 +31,18 @@ public class AICarMove : MonoBehaviour
         if (!PauseGame.gameIsPaused)
         {
             lifetime = carController.timer;
-            fuzzyLogic.evaluate = true;
+            
             distance = Vector3.Distance(transform.position, player.position);
-
+            
+            fuzzyLogic.evaluate = true;
+            
+            // Perform Fuzzification on the distance and previous lifetime
             fuzzyLogic.GetFuzzificationByName("distance").value = distance * 100f;
             fuzzyLogic.GetFuzzificationByName("previous_lifetime").value = previousLifetime;
 
+            // Defuzzify the output which is speed
             speed = fuzzyLogic.Output() * fuzzyLogic.defuzzification.maxValue * speedMultiplier;
+            
             transform.Translate(new Vector3(0f, 0f, -1f) * (speed * Time.deltaTime));
         }
     }
